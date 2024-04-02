5799993b872395f1d5f20f722db3cb5406bdfc90
ksprudhvi <91111944+ksprudhvi@users.noreply.github.com>
2024-04-03 01:28:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 settings = {
     'host': os.environ.get('ACCOUNT_HOST', 'https://scorecardgendb.documents.azure.com:443/'),
     'master_key': os.environ.get('ACCOUNT_KEY', 'FTUQgUomTG2bQwnva0OdHfLRxeUdBOvM9tGzOBtMciWYPyZsDsfW3mWLYU94KmlChAUNwxcRuj0MACDbfkyYcg=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'competition'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Container2'),
+    'competitionDB': os.environ.get('COSMOS_DATABASE', 'competition'),
+    'container1': os.environ.get('COSMOS_CONTAINER', 'Container2'),
 }
