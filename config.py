cb8e2a5d941470eb5a0e341102a0c6411fcbaacc
ksprudhvi <91111944+ksprudhvi@users.noreply.github.com>
2024-05-20 18:41:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://scorecardgendb.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'FTUQgUomTG2bQwnva0OdHfLRxeUdBOvM9tGzOBtMciWYPyZsDsfW3mWLYU94KmlChAUNwxcRuj0MACDbfkyYcg=='),
-    'competitionDB': os.environ.get('COSMOS_DATABASE', 'competition'),
-    'container1': os.environ.get('COSMOS_CONTAINER', 'Container2'),
+    'host': os.environ.get('ACCOUNT_HOST', 'https://scorecardgendata.documents.azure.com:443/'),
+    'master_key': os.environ.get('ACCOUNT_KEY', 'rJPINxAmYWh9j7ryJMyC9VjeCcSA9VGq2rOo49YM6r3suh3N1fQ38KoKIsNlIwS5bqVjERQtYpI3ACDb8WpECw=='),
+    'competitionDB': os.environ.get('COSMOS_DATABASE', 'EventCreator'),
+    'container1': os.environ.get('COSMOS_CONTAINER', 'EventMeta')
 }
